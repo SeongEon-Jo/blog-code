4c617670f6391b7737d93f0735f449ef2b920693
SeongEon-Jo <meanaccto@naver.com>
2024-02-27 23:28:42 +0900

    동시성 코드 구현부 수정

--- concurrent/src/main/java/com/concurrent/demo/BookCountService.java
+++ concurrent/src/main/java/com/concurrent/demo/BookCountService.java
@@ -3,23 +3,9 @@ package com.concurrent.demo;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BookCountService {
-
-    private int count;
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-    public int getCount() {
-        return this.count;
-    }
-
-    public synchronized void decreaseCount() {
-        this.count -= 1;
-    }
-
-    public synchronized void increaseCount() {
-        this.count += 1;
-    }
+public interface BookCountService {
+    void setCount(int count);
+    int getCount();
+    void decreaseCount();
+    void increaseCount();
 }
